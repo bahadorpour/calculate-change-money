e52d1d905fad29a3ff02882fcd8581d688664a78
Mojdeh Bahadorpour <bahadorpour@itq.de>
2021-01-03 19:58:47 +0100

    update app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,22 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CommingSoonComponent } from './components/comming-soon/comming-soon.component';
+import { AppComponent } from './app.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { PaymentAmountComponent } from './components/payment-amount/payment-amount.component';
 import { PayCashComponent } from './components/shared/pay-cash/pay-cash.component';
+import { SaleComponent } from './components/shared/sale/sale.component';
 
 export const routes: Routes = [
-  { path: '', component: PaymentAmountComponent },
-  { path: 'payCash', component: PayCashComponent },
-  { path: 'commingSoon', component: CommingSoonComponent },
+  { path: '', redirectTo: 'cash', pathMatch: 'full' },
+  { path: 'cash', component: PayCashComponent },
+  { path: 'comingSoon', component: ComingSoonComponent },
+  { path: 'electronicCash', redirectTo: 'comingSoon', pathMatch: 'full' },
+  { path: 'creditCard', redirectTo: 'comingSoon', pathMatch: 'full' },
+  { path: 'voucher', redirectTo: 'comingSoon', pathMatch: 'full' },
+  { path: 'others', redirectTo: 'comingSoon', pathMatch: 'full' },
 
   { path: '**', component: NotFoundComponent } // Wildcard route for a 404 page
 ];
