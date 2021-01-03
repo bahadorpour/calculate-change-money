c05e0fb8fc6cb875cc41f1cb7373816e59816d84
Mojdeh Bahadorpour <bahadorpour@itq.de>
2021-01-03 19:59:07 +0100

    create data service

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Button } from '../models/Button';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  checkoutButtons: Button[];
+
+  constructor() {
+    this.checkoutButtons = [
+      {
+        id: 1,
+        route: "cash",
+        label: "Bar"
+      },
+      {
+        id: 2,
+        route: "electronicCash",
+        label: "EC"
+      },
+      {
+        id: 3,
+        route: "creditCard",
+        label: "Kreditkarte"
+      },
+      {
+        id: 4,
+        route: "voucher",
+        label: "gutschein"
+      },
+      {
+        id: 5,
+        route: "others",
+        label: "Sonstige"
+      }
+    ];
+  }
+
+  /**
+   * Return buttons data
+   */
+  getButtons(): Button[] {
+    return this.checkoutButtons;
+  }
+
+}
